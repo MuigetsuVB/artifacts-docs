2d355c7d9f22d563a79656ede6ea5ecfa302cc7b
Vincent B <muigetsu@hotmail.com>
2023-10-14 13:38:03 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span><img src="https://api.artifactsmmo.com/images/docs/small-logo.png" alt="logo" /></span>,
