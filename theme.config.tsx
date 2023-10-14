a0d8c766258682e54f2f4f01e3045a0b8e506265
Vincent B <muigetsu@hotmail.com>
2023-10-14 00:46:51 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Artifacts Docs</span>,
-  project: {
-    link: 'https://artifactsmmo.com/',
-  },
   chat: {
     link: 'https://discord.gg/prEBQ8a6Vs',
   },
-  docsRepositoryBase: 'https://github.com/MuigetsuVB/mmo-docs/',
+  docsRepositoryBase: 'https://github.com/MuigetsuVB/mmo-docs/blob/main/',
   footer: {
     text: '© 2023 Artifacts. All rights reserved. ',
   },
