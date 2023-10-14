7ee99d3e2263295262103162db8f28d6e9692563
Vincent B <muigetsu@hotmail.com>
2023-10-14 13:49:05 -0400

    123

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span><img src="https://api.artifactsmmo.com/images/docs/small-logo.png" alt="logo" /></span>,
   chat: {
     link: 'https://discord.gg/prEBQ8a6Vs',
   },
-  titleTemplate: 'Artifacts Docs',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Artifacts'
+      }
+    }
+  },
   docsRepositoryBase: 'https://github.com/MuigetsuVB/mmo-docs/blob/main/',
   footer: {
     text: '© 2023 Artifacts. All rights reserved. ',
