cc34bcb82b691d90e2dd409e4d9b69a25e96b8f5
Vincent B <muigetsu@hotmail.com>
2023-10-14 13:34:39 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span><img src="https://api.artifactsmmo.com/images/docs/small-logo.png" alt="logo" /></span>,
   chat: {
     link: 'https://discord.gg/prEBQ8a6Vs',
   },
-  head: () => {
-    const { frontMatter } = useConfig()
-    return (
-      <>
-        <meta property="og:title" content={frontMatter.title || 'Artifacts'} />
-        <meta
-          property="og:description"
-          content={frontMatter.description}
-        />
-      </>
-    )
-  },
+  title: 'Artifacts MMO',
   docsRepositoryBase: 'https://github.com/MuigetsuVB/mmo-docs/blob/main/',
   footer: {
     text: '© 2023 Artifacts. All rights reserved. ',
