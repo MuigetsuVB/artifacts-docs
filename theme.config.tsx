7a80e4c9a2ba7f28d47a07cec7a902c2f93c80b5
muigetsu <muigetsu@hotmail.com>
2024-07-31 17:10:15 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
-import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span><img src="/logo.png" alt="logo" /></span>,
+  logo: <span><img src="https://api.artifactsmmo.com/images/docs/small-logo.png" alt="logo" /></span>,
   chat: {
     link: 'https://discord.gg/prEBQ8a6Vs',
   },
@@ -36,7 +35,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="/favicon.png" />
+      <link rel="icon" href="https://api.artifactsmmo.com/images/docs/small-logo.png" />
       <meta property="og:title" content="Artifacts - Documentation" />
       <meta property="og:description" content="Artifacts is a API-based MMORPG game. Use any programming language to control your characters with the API." />
     </>
